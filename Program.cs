c02adfcccdbb82cd4195a62834048334fa0ecbce
AlekseiPavlovich <83204001+MrParanoic@users.noreply.github.com>
2021-06-04 22:57:17 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Bar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int barProcent = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+                drawBar(barProcent);
+            }
+        }
+        static void drawBar(int bar) 
+        {
+            char[] healgh = {' ',' ',' ',' ',' ',' ',' ', ' ',' ',' ' };
+            Console.Write("[");
+            Console.BackgroundColor = ConsoleColor.Red;
+            for (int i = 0; i <= healgh.Length; i++)
+            {
+                if (i < bar)
+                {
+                    Console.Write(healgh[i]);
+                }
+                if (i > bar)
+                {
+                    Console.ResetColor();
+                    Console.Write("_");
+                }
+            }
+            Console.Write("]");
+            Console.Write("[");
+            Console.BackgroundColor = ConsoleColor.Blue;
+            for (int i = 0; i <= healgh.Length; i++)
+            {
+                if (i < bar)
+                {
+                    Console.Write(healgh[i]);
+
+                }
+                if (i > bar)
+                {
+                    Console.ResetColor();
+                    Console.Write("_");
+                }
+            }
+            Console.Write("]");
+
+        }
+    }
+}
